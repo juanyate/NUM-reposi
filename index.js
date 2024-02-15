b4e5d3afc501a7327d6d90c67740db21ab01c794
carlos <jc0900518@gmail.com>
2024-02-15 15:09:30 -0500

    mi segundo commit

--- index.js
+++ index.js
@@ -3,4 +3,19 @@ function getPetExerciseInfo() {
         return prompt("Tipo de mascota (perro, gato, ave)");
     }
     
+    function imprimirTipoMascota(tipo) {
+        switch (tipo) {
+            case "perro":
+                console.log("Es un perro");
+                break;
+            case "gato":
+                console.log("Es un gato");
+                break;
+            case "ave":
+                console.log("Es un ave");
+                break;
+            default:
+                console.log("Tipo de mascota no válido");
+        }
+    }
 }
